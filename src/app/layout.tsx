3e33a57cc11e5474f3f82552e52fbac44e91058b
Clark Kent Renojo <CRenojo@gmail.com>
2025-06-26 06:24:00 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,13 +17,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="h-full" suppressHydrationWarning>
+    <html lang="en" className="h-full">
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400..900;1,400..900&family=PT+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet" />
       </head>
-      <body className={cn('font-body antialiased h-full flex flex-col')}>
+      <body className={cn('font-body antialiased h-full flex flex-col')} suppressHydrationWarning>
         <CartProvider>
           <Header />
           <main className="flex-grow">{children}</main>
